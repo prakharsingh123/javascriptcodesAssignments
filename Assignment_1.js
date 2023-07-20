fc8cf46d39b27b8a94340c16ef80d983fb3fb69c
prakharsingh123 <102253372+prakharsingh123@users.noreply.github.com>
2023-07-20 18:13:20 +0530

    Create Assignment_1.js

---
+++ Assignment_1.js
@@ -0,0 +1,8 @@
+let markMass= 45;
+let johnMass = 58;
+let mark_H= 170;
+let john_H= 120;
+let markBMI= markMass/(mark_H**2);
+let johnBMI= johnMass/(john_H**2);
+let isgreater= markBMI<johnBMI;
+console.log(isgreater);
